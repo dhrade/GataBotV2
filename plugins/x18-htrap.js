ce017818374a1c912e18c378a60e49add6c12f9f
dhrade <dhrade@gmail.com>
2022-06-09 17:16:44 -0500

    Update x18-htrap.js

--- plugins/x18-htrap.js
+++ plugins/x18-htrap.js
@@ -1,3 +1,4 @@
+/*
 let axios = require("axios")
 let handler = async (m, { conn }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'   
@@ -28,3 +29,4 @@ handler.exp = 200
 handler.limit = false
 
 module.exports = handler
+*/
