2d7b0ff718e8cff8a50f1ee24b638c5cc3ad3569
dhrade <dhrade@gmail.com>
2022-06-09 18:03:48 -0500

    Update x18-yuri.js

--- plugins/x18-yuri.js
+++ plugins/x18-yuri.js
@@ -1,3 +1,4 @@
+/*
 const axios = require('axios')
  let handler = async(m, { conn }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '❰ ⚠️ ❱ *Función Nsfw Desactivada*\n*Escriba #on nsfw para activar esta Función.*'
@@ -22,3 +23,4 @@ handler.exp = 200
 handler.limit = false
 
 module.exports = handler
+*/
